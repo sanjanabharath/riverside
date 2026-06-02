1aba7f649328a01f2ecfd49ccaa1d11aad24cdbc
sanjanabharath <sanjanabarath@gmail.com>
2026-06-02 10:31:07 +0530

    Added cors

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -1,12 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", enableCORS(homeHandler))
+
+	log.Println("Server running on server '8080'")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error while starting the server")
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
+}
+
+func enableCORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Header", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
+	}
 }
